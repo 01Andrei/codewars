735d20854af01520d9f064b484dc6c1186784af5
01Andrei <mail@gmail.compost1234@tut.by>
2021-10-19 13:15:16 +0300

    init: create task str to num
    performed task str t nmb

---
+++ tasks/convert str num/convert str num.js
@@ -0,0 +1,10 @@
+//Convert a String to a Number!
+var stringToNumber = function(str){
+    return +str;
+}
+
+/* return parseInt(str);
+   return Number(str); 
+   const stringToNumber = str => Number(str);
+   return str*1;
+*/
