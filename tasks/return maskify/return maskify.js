0c35156c0c9477774198d69b0d3af5e905b1c6e8
01Andrei <mail@gmail.compost1234@tut.by>
2021-10-20 18:08:40 +0300

    init: create maskify task
    maskify task well done

---
+++ tasks/return maskify/return maskify.js
@@ -0,0 +1,11 @@
+//codeears maskify
+// return masked string
+function maskify(cc) {
+    let masked = "";
+    let lastFour = cc.substr(-4);
+      for (var i = 0; i < cc.length-4; i++){
+        masked+="#"
+      }
+      return masked + lastFour;
+    }
+    
